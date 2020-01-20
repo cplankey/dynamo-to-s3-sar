e9b2d9c36dcf2672d498a70212dc6fababd9bf49
Chris Plankey <plankey5@aol.com>
2020-01-20 15:07:48 -0500

    cleanup

--- dynamo-to-s3/app.js
+++ dynamo-to-s3/app.js
@@ -10,65 +10,51 @@ let response;
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - Success or failure messages and status codes
+ * @returns {Object} object - Success or error messages
  * 
  */
 const AWS = require('aws-sdk');
-var docClient = new AWS.DynamoDB.DocumentClient();
-var s3 = new AWS.S3();
-var dynamoContents = [];
-var params = {
-    TableName: process.env.TABLE_NAME
-};
+const s3 = new AWS.S3();
+const docClient = new AWS.DynamoDB.DocumentClient();
 exports.lambdaHandler = async (event, context) => {
+    let tableContents;
     try{
         //get items from dynamo
-        docClient.scan(params, dynamoCallback);
+        const params = {
+            TableName: process.env.TABLE_NAME,
+        };
+        tableContents = await scanDB(params);
     }catch{
         console.log(err);
         return err;
     }
     try{
-        //write items to s3
-        var params = {
-            Body: new Buffer(dynamoContents, 'binary'),
+        //write items to s3 using timestamp
+        let fileName = new Date().toISOString() + '.json';
+        var s3Params = {
             Bucket: process.env.BUCKET_NAME,
-            Key: new Date().toISOString() + '.json',
+            Key: fileName,
+            Body: JSON.stringify(tableContents),
+            ContentType: "application/json"
            };
-           s3.putObject(params, function(err, data) {
-             if (err) throw err; // an error occurred
-             else     {
-                 console.log(data);         // successful response
-                 response = {
-                    'statusCode': 200,
-                    'body': JSON.stringify({
-                        message: 'Backup and Upload Successful',
-                    })
-                }
-             }
-           });
+           await s3.putObject(s3Params).promise();
+           response = {
+               statusCode: 200,
+               message: `Successfully wrote contents from ${process.env.TABLE_NAME} to ${process.env.BUCKET_NAME}/${fileName}`
+           };
     }catch{
         console.log(err);
         return err;
     }
-
-    return response
+    return response;
 };
-
-function dynamoCallback(err, data) {
-    if (err) {
-        console.error("Unable to scan:: ", JSON.stringify(err));
-    } else {
-        console.log("Scanned");
-        data.Items.forEach(function(item) {
-           console.log("Item : ",JSON.stringify(item));
-           dynamoContents.push(item);
-        });
-        // continue scanning if more keys
-        if (typeof data.LastEvaluatedKey != "undefined") {
-            console.log("More items to scan");
-            params.ExclusiveStartKey = data.LastEvaluatedKey;
-            docClient.scan(params, dynamoCallback);
-        }
-    }
-}
+async function scanDB(params) {
+    let dynamoContents = [];
+    let items;
+    do{
+        items =  await docClient.scan(params).promise();
+        items.Items.forEach((item) => dynamoContents.push(item));
+        params.ExclusiveStartKey  = items.LastEvaluatedKey;
+    }while(typeof items.LastEvaluatedKey != "undefined");
+    return dynamoContents;
+};
